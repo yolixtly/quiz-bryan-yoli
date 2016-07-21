547491cc99e4236f0270375f94d0b098a6708bbc
bjf2015 <bfranzoi@icloud.com>
2016-07-21 12:49:48 -0700

    Saving afternoon progress

--- app.js
+++ app.js
@@ -6,18 +6,64 @@
 	//button next
 	//Scoreboard section: feedback '1 question of 5' 
 
-
-
 /*JS section */
 
+//declaring variables 
+
+var form,
+htmlQuestion,
+htmlAnswers,
+htmlScore,
+button,
+newQuestion,
+questionCount,
+questionGroup;
+;
+
+//fetch DOM elements
+
+form = $('#record-question');
+htmlQuestion = $('#question-wrapper');
+htmlAnswers = $('#answer-wrapper');
+htmlScore = $('#scoreboard');
+button = $('#button-area');
+
+//function that holds the initial values of the game and starts the game 
+startGame();
+
+function startGame(){
+	questionCount = 0;
+	renderQuestion(questionCount);
+}
+
+
+//function that dynamically adds each question to HTML or DOM
+function renderQuestion(currentQuestion) {
+	console.log(questionGroup);
+	newQuestion = '<h1 id="question-area">' + questionGroup[questionCount].questionText +  '</h1>';
+	htmlQuestion.append(newQuestion);
+}
+//function that dynamically adds all answers to HTML or DOM
+function renderAnswers(currentQuestion) {
+	// var showChoices = <form id="record-question">;
+	// $.each
+	//htmlAnswers = '<input id="answer-area" type="radio"><label for="answer-area">'+ questionGroup[questionCount].potentialAnswer + '</label>';
+
+}
+//adding functionality to the button - button will submit a new and increase the question count
+
+button.on('click', function(){
+	questionCount++;
+	console.log(questionCount);
+})
 // Data Base Store our questions
 		//create a reusable object to pass all our questions
 
 function Question(questionText, arrayOfAnswers,correctAnswer){
-	this.questionText: questionText,
+	this.questionText= questionText;
 //array every question is an element of an array
-	this.potentialAnswer: arrayOfAnswers,
-	this.correctAnswer: correctAnswer,
+	this.potentialAnswer= arrayOfAnswers;
+	this.correctAnswer= correctAnswer;
 }
 
 
@@ -25,11 +71,23 @@ function Question(questionText, arrayOfAnswers,correctAnswer){
 questionGroup = new Array();
 
 //instantiate the question object
+// array of objects 
+//questions (object)
+//answers
+//correct answer
+
 questionGroup[0] = new Question(
 	'Which answer below is NOT a common way to instantiate an object',
+
 	['Obect.create()', 'Scaffolding', 'Factory functions', 'Constructor functions'],
+
 	[1]
 	);
+
+
+
+
+
 questionGroup[1] = new Question(
 	'What will a Constructor always begin with?',
 	['.', 'lower-case letter', '@', 'upper-case letter'],
@@ -66,27 +124,25 @@ questionGroup[7] = new Question(
 	[2]
 	);
 questionGroup[8] = new Question(
-	'What must you do each night in order to be successful with Thinkful',
-	['speak with your mentor', 'complete daily refections', 'complete assigned nightly reading', 'all of the above'],
-	[3]
+	'Where should you put your <script> tags in your HTML page',
+	['In the header', 'before you close your body tags', 'at the end of the page', 'at the start of the page'],
+	[1]
 	);
 questionGroup[9] = new Question(
-	'What must you do each night in order to be successful with Thinkful',
-	['speak with your mentor', 'complete daily refections', 'complete assigned nightly reading', 'all of the above'],
-	[3]
+	'which of the following operators uses coercion?',
+	['=', '==', '===', 'all of the above'],
+	[1]
 	);
 questionGroup[10] = new Question(
-	'What must you do each night in order to be successful with Thinkful',
-	['speak with your mentor', 'complete daily refections', 'complete assigned nightly reading', 'all of the above'],
-	[3]
+	'What is hoisting',
+	['moving up your HTML tags', 'moving declarations to the top in javascript', 'moving declarations to the bottom in javascript', 'a way to cleanup your CSS code'],
+	[1]
 	);
 
+//render questions dynamically
 
-
-		// array of objects 
- 		//questions (object)
- 		//answers
- 		//correct answer
+		
+ 		
 
 
 
@@ -106,8 +162,8 @@ questionGroup[10] = new Question(
 
 // //Constructor object
 // function Human() {
-// 	legs : 2,
-// 	arms : 2,
+// 	legs : 2;
+// 	arms : 2;
 // }
 
 // var Yoli = new Human; 
@@ -116,6 +172,10 @@ questionGroup[10] = new Question(
 
 
 
+// parameter : local variable , its a placeholder
+// //create a function test(parameter)
+// argument : actual value that we want to pass instead of a parameter
+// //call function:  test(0)
 
 
 
